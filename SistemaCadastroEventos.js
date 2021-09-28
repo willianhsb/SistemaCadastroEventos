a2580cea51025721349132c05851470cd78a4f5e
willian <willianhsb@gmail.com>
2021-09-27 23:33:57 -0300

    segunda alteração sistemacadastroevento.js

--- SistemaCadastroEventos.js
+++ SistemaCadastroEventos.js
@@ -1,62 +1,48 @@
 let readlineSync = require('readline-sync');
 var nomeEvento;
 var nomeParticipante;
-var idadeParticipante;
+var idadeParticipante = 0;
 var dataEvento;
 var dataAtual;
-var qtdeInscritos;
-var novoParticipante;
+var qtdeInscritos = 98;
+var novoParticipante = "sim";
 
-    nomeEvento = readlineSync.question("Informe o Nome do Evento: ");
-    dataAtual= readlineSync.question("Informe a Data de Hoje: ");
-    dataEvento = readlineSync.question("Informe a Data do Evento: ");
+nomeEvento = readlineSync.question("Informe o Nome do Evento: ");
+dataAtual = readlineSync.question("Informe a Data de Hoje: ");
+dataEvento = readlineSync.question("Informe a Data do Evento: ");
+
+if (dataEvento >= dataAtual) {
     
 
-while(dataAtual <= dataEvento ){   
+    do {
+        if (qtdeInscritos <= 100) {
 
-    console.log("Cadastro não permitido. Informe uma data anterior a data do evento");
+            qtdeInscritos++;
+            
+            nomeParticipante = readlineSync.question("Informe o Nome do Participante: ");
+            idadeParticipante = readlineSync.question("Informe a Idade do Participante: ");
+            if (idadeParticipante >= 18) {
+                console.log("Participante Maior de Idade. Cadastro Permitido!!");
 
-    dataAtual= readlineSync.question("Informe a Data de Hoje: ");  
+            } else {
+                console.log("Participante Menor de Idade. Cadastro Não Permitido!!");
+            }
+
+
+        } else {
+            console.log("Cadastro de Evento Não Permitido. Quantidade Maxima de Participantes Atingida!");
+           
+        } novoParticipante = readlineSync.question("Cadastrar Novo Participante?(sim/nao)");
+
+    } while (novoParticipante = "sim");
+
+} else {
+    console.log("Cadastro de Evento Não Permitido");
+
 }
-       
-        
 
-            
-                for(qtdeInscritos = 98; qtdeInscritos <= 100; qtdeInscritos++){
 
-                    
 
-                    if(qtdeInscritos >= 1){
 
-                     novoParticipante = readlineSync.question("Deseja Cadastrar Novo Participante? Sim(s) / Nao(n): ");
 
-                    while(novoParticipante = "s"){
-                
 
-                    if(qtdeInscritos => 100){            
-    
-                    nomeParticipante = readlineSync.question("Informe o Nome do Participante: ");
-                    idadeParticipante = readlineSync.question("Informe a Idade do Participante: ");
-    
-                    while(idadeParticipante <= 18){
-                    console.log("Cadastro Não Permitido. Participante Menor de Idade");
-                    idadeParticipante = readlineSync.question("Informe a Idade do Participante: ");
-    
-                    }                   
-                    
-                     qtdeInscritos++;
-                   
-                    }else{
-                        console.log("Cadastro não permitido. Quantidade Maxima de incritos atingida")
-                    }
-                    
-                }
-            }
-                
-            }
-            console.log("Cadastro Finalizado")
-                    
-            
-        
-
-    
